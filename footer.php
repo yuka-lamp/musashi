14d087f89f673368c97589770252c89983f88926
yuka-lamp <oonishi@lamp.jp>
2020-12-22 16:33:21 +0900

    header/footer

--- footer.php
+++ footer.php
@@ -4,14 +4,65 @@ $wp_url = get_template_directory_uri(); ?>
 
 </main>
 
-<footer class="py-5">
-<div class="container">
-<ul>
-  <li>
-    <a class="link-arrow" href="#">会社紹介</a>
-  </li>
-</ul>
-</div>
+<footer class="footer">
+  <div class="container main">
+    <div class="d-md-flex d-block justify-content-between">
+      <!-- ▼ 会社情報 -->
+      <div class="footer__info col-md-6">
+        <img src="<?php echo $wp_url ?>/dist/images/logo.png" alt="寿司のむさしのロゴ" srcset="<?php echo $wp_url ?>/dist/images/logo.png 1x, <?php echo $wp_url ?>/dist/images/logo@2x.png 2x">
+        <div class="footer__info__address">
+          <div class="row mb-2">
+            <p class="col-md-4 text-info m-0">三条本店</p>
+            <p class="col-md-8 text-info m-0">京都市中京区河原町通三条上ル恵比須町440</p>
+          </div>
+          <div class="row mb-2">
+            <p class="col-md-4 text-info m-0">京都駅八条口店</p>
+            <p class="col-md-8 text-info m-0">京都市下京区東塩小路高倉町8-3京都駅八条口構内 アスティロード京都おもてなし小路</p>
+          </div>
+          <div class="row">
+            <p class="col-md-4 text-info m-0">キッチンセンター</p>
+            <p class="col-md-8 text-info m-0">京都市北区堀川通北山上ル</p>
+          </div>
+        </div>
+        <ul class="footer__info__link m-0 p-0">
+          <li class="d-inline mr-3">
+            <a class="link-arrow text-info" href="<?php echo $home ?>/company/">会社紹介</a>
+          </li>
+          <li class="d-inline">
+            <a class="link-arrow text-info" href="<?php echo $home ?>/contact/">お問い合わせ</a>
+          </li>
+        </ul>
+      </div>
+      <!-- ▲ 会社情報 -->
+      <!-- ▼ SNSリンクボタン -->
+      <div class="footer__sns col-md-3 text-right">
+        <a class="d-inline-block mr-3" href="" target="_blank">
+          <img src="<?php echo $wp_url ?>/dist/images/icon_fb.png" alt="Facebook" srcset="<?php echo $wp_url ?>/dist/images/icon_fb.png 1x, <?php echo $wp_url ?>/dist/images/icon_fb@2x.png 2x">
+        </a>
+        <a class="d-inline-block" href="" target="_blank">
+          <img src="<?php echo $wp_url ?>/dist/images/icon_inst.png" alt="Instagram" srcset="<?php echo $wp_url ?>/dist/images/icon_inst.png 1x, <?php echo $wp_url ?>/dist/images/icon_inst@2x.png 2x">
+        </a>
+      </div>
+      <!-- ▲ SNSリンクボタン -->
+    </div>
+    <!-- ▼ トップへ戻る -->
+    <div class="footer__pagetop">
+      <a href="#top">
+        <img src="<?php echo $wp_url ?>/dist/images/footer_pagetop.png" alt="Instagram" srcset="<?php echo $wp_url ?>/dist/images/footer_pagetop.png 1x, <?php echo $wp_url ?>/dist/images/footer_pagetop@2x.png 2x">
+      </a>
+    </div>
+    <!-- ▲ トップへ戻る -->
+  </div>
+  <!-- ▼ コピーライト + プライバシーポリシー -->
+  <div class="footer__small bg-dark py-2">
+    <div class="container">
+      <div class="d-md-flex d-block justify-content-between">
+        <a class="small text-white" href="<?php echo $home ?>/privacy-policy/">プライバシーポリシー</a>
+        <p class="small text-white m-0">2020 (C) copyright All reserved.SUSHINOMUSASHI</p>
+      </div>
+    </div>
+  </div>
+  <!-- ▲ コピーライト + プライバシーポリシー -->
 </footer>
 
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -11,26 +11,70 @@ $wp_url = get_template_directory_uri(); ?>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<header class="header">
-  <nav class="header__nav site-header sticky-top">
-  <div class="container d-flex flex-column flex-md-row mt-4">
-    <div class="header__nav-inner">
-      <a href="<?php echo $home ?>">
-        <h1 class="" href="#">
-          <img class="" style="width: 195.37px; height:58.81px;" src="<?php echo $wp_url ?>/dist/images/logo.png" alt="<?php get_the_title() ?>" srcset="<?php echo $wp_url ?>/dist/images/logo.png 1x, <?php echo $wp_url ?>/dist/images/logo@2x.png 2x">
-        </h1>
-      </a>
-      <div class="header__nav-list ml-md-auto">
-        <a class="header__nav-list-item d-md-inline-block text-white" href="<?php echo $home ?>/about/">こだわり<span>about</span></a>
-        <a class="header__nav-list-item d-md-inline-block text-white" href="<?php echo $home ?>/menu/">お品書き<span>menu</span></a>
-        <a class="header__nav-list-item d-md-inline-block text-white" href="<?php echo $home ?>/about/">こだわり<span>about</span></a>
-        <a class="header__nav-list-item d-md-inline-block text-white" href="<?php echo $home ?>/menu/">お品書き<span>menu</span></a>
+<header class="header w-100 bg-white">
+    <nav class="navbar navbar-expand-lg w-100">
+      <div class="w-100">
+        <div class="d-block d-lg-flex align-items-center">
+          <!-- ▼ メイン -->
+          <div>
+            <h1 class="d-inline-block navbar-brand mr-4 p-0">
+              <a class="d-inline-block " href="<?php echo $home ?>">
+              <img class="navbar-brand-img" src="<?php echo $wp_url ?>/dist/images/logo.png" alt="<?php get_the_title() ?>" srcset="<?php echo $wp_url ?>/dist/images/logo.png 1x, <?php echo $wp_url ?>/dist/images/logo@2x.png 2x">
+              </a>
+            </h1>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#Navber" aria-controls="Navber" aria-expanded="false" aria-label="ナビゲーションの切替">
+              <span class="navbar-toggler-icon"></span>
+              <p class="mb-0 text-white font-weight-bold">メニュー</p>
+            </button>
+          </div>
+          <!-- ▲ メイン -->
+          <!-- ▼ ページ一覧 -->
+          <div class="collapse navbar-collapse bg-white" id="Navber">
+            <ul class="navbar-nav">
+              <li class="nav-item d-lg-inline-block pl-4 pr-4">
+                <a class="nav-link" href="<?php echo $home ?>/about/">こだわり<span class="en">about</span></a>
+              </li>
+              <li class="nav-item d-lg-inline-block pl-4 pr-4">
+                <a class="nav-link" href="<?php echo $home ?>/menu/">お品書き<span class="en">menu</span></a>
+              </li>
+              <li class="nav-item d-lg-inline-block pl-4 pr-4">
+                <a class="nav-link" href="<?php echo $home ?>/shop/">店舗紹介<span class="en">shop</span></a>
+              </li>
+              <li class="nav-item d-lg-inline-block pl-4">
+                <a class="nav-link" href="<?php echo $home ?>/news/">むさしニュース<span class="en">news</span></a>
+              </li>
+            </ul>
+          </div>
+          <!-- ▲ ページ一覧 -->
+        </div>
+        <!-- ▼ ボタン(/athome/) -->
+        <a class="header__btn mincho" href="<?php echo $home ?>/athome/">
+          <img class="header__btn-icon" src="<?php echo $wp_url ?>/dist/images/btn_takeout_icon.png" alt="お持ち帰りアイコン" srcset="<?php echo $wp_url ?>/dist/images/btn_takeout_icon.png 1x, <?php echo $wp_url ?>/dist/images/btn_takeout_icon@2x.png 2x">お持ち帰り<br class="d-block d-lg-none"><span class="d-lg-block d-none">・</span>宅配
+        </a>
+        <!-- ▲ ボタン(/athome/) -->
       </div>
-    </div>
-    <a href="header__nav-btn"><img class="" style="width: 32.52px; height:29.2px;" src="<?php echo $wp_url ?>/dist/images/btn_takeout_icon.png" alt="お持ち帰りアイコン" srcset="<?php echo $wp_url ?>/dist/images/btn_takeout_icon.png 1x, <?php echo $wp_url ?>/dist/images/btn_takeout_icon@2x.png 2x">お持ち帰り・宅配</a>
-  </div>
   </nav>
 </header>
 <!-- /header -->
 
-<main>
+<main id="top">
+
+<?php if (is_front_page()): ?>
+
+<!-- ▼ トップページのメインビジュアル -->
+<section class="mv">
+
+</section>
+<!-- ▲ トップページのメインビジュアル -->
+
+<?php elseif (is_archive()) : ?>
+
+<!-- ▼ 下層ページのメインビジュアル -->
+<section class="submv">
+  <div class="container">
+
+  </div>
+</section>
+<!-- ▲ 下層ページのメインビジュアル -->
+
+<?php endif; ?>
