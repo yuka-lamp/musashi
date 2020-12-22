9a51f719aae2b11792682d1970d555d1e336b769
yuka-lamp <oonishi@lamp.jp>
2020-12-22 11:54:07 +0900

    common

--- front-page.php
+++ front-page.php
@@ -2,13 +2,19 @@
 $home = esc_url(home_url());
 $wp_url = get_template_directory_uri();
 get_header(); ?>
-<div class="position-relative overflow-hidden text-center bg-light">
-<div class="col-md-5 p-lg-5 mx-auto my-5">
-<h1 class="display-4 font-weight-normal">Punny headline</h1>
-<p class="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple’s marketing pages.</p>
-<a class="btn btn-outline-secondary" href="#">Coming soon</a>
-</div>
-<div class="product-device shadow-sm d-none d-md-block"></div>
-<div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
-</div>
+
+<section class="top__concept">
+  <div class="container text-center">
+    <h2 class="ttl-2 mb-5">むさしのおもい<span class="ttl-2-en en">concept</span></h2>
+    <p class="mt-4 pb-3 lh-large text-center mincho">ネタは勿論、米や酢、お茶や醤油、わさび、海苔、それに店内の清潔さ、
+    <br class="d-none d-md-block">スタッフの気配り心配りと細部にまでしっかりこだわっています。
+    <br class="d-none d-md-block">むさしのすしは、1977年創業以来、独自の美味への道を貫き、
+    <br class="d-none d-md-block">オリジナルの食材、調理、調味にこだわりつづけています。
+    <br class="d-none d-md-block">その基本にあるのが真心です。</p>
+    <a class="link-arrow mt-5 f-18 mincho" href="#">詳しくみる</a>
+  </div>
+</section>
+
+<a class="btn-primary" href="#"><span>店舗一覧を見る</span></a>
+
 <?php get_footer();
--- header.php
+++ header.php
@@ -11,21 +11,25 @@ $wp_url = get_template_directory_uri(); ?>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<header>
-<nav class="site-header sticky-top py-1 bg-dark">
-<div class="container d-flex flex-column flex-md-row">
-<a class="py-2 text-white" href="#" aria-label="Product">LOGO</a>
-<div class="ml-md-auto">
-<a class="p-2 d-none d-md-inline-block text-white" href="#">Tour</a>
-<a class="p-2 d-none d-md-inline-block text-white" href="#">Product</a>
-<a class="p-2 d-none d-md-inline-block text-white" href="#">Features</a>
-<a class="p-2 d-none d-md-inline-block text-white" href="#">Enterprise</a>
-<a class="p-2 d-none d-md-inline-block text-white" href="#">Support</a>
-<a class="p-2 d-none d-md-inline-block text-white" href="#">Pricing</a>
-<a class="p-2 d-none d-md-inline-block text-white" href="#">Cart</a>
-</div>
-</div>
-</nav>
+<header class="header">
+  <nav class="header__nav site-header sticky-top">
+  <div class="container d-flex flex-column flex-md-row mt-4">
+    <div class="header__nav-inner">
+      <a href="<?php echo $home ?>">
+        <h1 class="" href="#">
+          <img class="" style="width: 195.37px; height:58.81px;" src="<?php echo $wp_url ?>/dist/images/logo.png" alt="<?php get_the_title() ?>" srcset="<?php echo $wp_url ?>/dist/images/logo.png 1x, <?php echo $wp_url ?>/dist/images/logo@2x.png 2x">
+        </h1>
+      </a>
+      <div class="header__nav-list ml-md-auto">
+        <a class="header__nav-list-item d-md-inline-block text-white" href="<?php echo $home ?>/about/">こだわり<span>about</span></a>
+        <a class="header__nav-list-item d-md-inline-block text-white" href="<?php echo $home ?>/menu/">お品書き<span>menu</span></a>
+        <a class="header__nav-list-item d-md-inline-block text-white" href="<?php echo $home ?>/about/">こだわり<span>about</span></a>
+        <a class="header__nav-list-item d-md-inline-block text-white" href="<?php echo $home ?>/menu/">お品書き<span>menu</span></a>
+      </div>
+    </div>
+    <a href="header__nav-btn"><img class="" style="width: 32.52px; height:29.2px;" src="<?php echo $wp_url ?>/dist/images/btn_takeout_icon.png" alt="お持ち帰りアイコン" srcset="<?php echo $wp_url ?>/dist/images/btn_takeout_icon.png 1x, <?php echo $wp_url ?>/dist/images/btn_takeout_icon@2x.png 2x">お持ち帰り・宅配</a>
+  </div>
+  </nav>
 </header>
 <!-- /header -->
 
